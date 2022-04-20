e8b413c92d22f4884ae586a9aa31dbbb2c0f10c4
Yohani <yohani@MacBook-Air.hsd1.fl.comcast.net>
2022-04-20 03:36:14 -0400

    Settings ready

--- api_rest/settings.dist.py
+++ api_rest/settings.dist.py
@@ -153,13 +153,10 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_datatables.renderers.DatatablesRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework_datatables.filters.DatatablesFilterBackend',
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework_datatables.pagination.DatatablesPageNumberPagination',
     #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
     #'PAGE_SIZE': 100,
